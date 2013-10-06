2c32990ec5e2075c20dbc410ff9afc150b21b75d
ClementRoy <mer02ma>
2013-10-06 18:15:14 +0200

    template

---
+++ template.php
@@ -0,0 +1,16 @@
+
+<?php require('parts/header.php'); ?>
+<?php require('parts/menu.php'); ?>
+
+	<div class="container">
+
+		<div class="row content">
+			<h1>Display homepage widgets here</h1>
+		</div>
+
+
+
+	</div><!-- /.container -->
+
+
+<?php require('parts/footer.php'); ?>
