11bb00207ab0faf6d4b3bac5b2d84b429f4e2e4d
Christophe Béghin <mail@christophebeghin.fr>
2013-10-06 16:04:00 +0200

    index

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+<<<<<<< HEAD
+	require 'config/settings.php';
+	require 'config/mysql.php';
+?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+	<div class="row">
+	  <div class="col-md-4">.col-md-1</div>
+	  <div class="col-md-4">.col-md-1</div>
+	  <div class="col-md-4">.col-md-1</div>
+	</div>
+	<div class="row">
+	  <div class="col-md-4">.col-md-1</div>
+	  <div class="col-md-4">.col-md-1</div>
+	  <div class="col-md-4">.col-md-1</div>
+	</div>
+</div>
+
+	Gite-des-cimes
+	==============
+
+
+	Gîte des Cîmes
+	tdhdhd
+
+<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+</body>
+</html>
+=======
+
+echo "hello";
+
+?>
+=======
+
+echo "hello";
+
+?>
+>>>>>>> ee0546c2da4f1fff4308ffd2c29030eb3aae2ad7
