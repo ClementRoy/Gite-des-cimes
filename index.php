a8f597cd77c78dd0770c9cc494e8dcca251dfe9c
Christophe Béghin <mail@christophebeghin.fr>
2013-10-06 17:30:35 +0200

    index.php

--- index.php
+++ index.php
@@ -4,9 +4,82 @@
 
 	<div class="container">
 
-	  <div class="content">
-	    <h1>Display homepage widgets here</h1>
-	  </div>
+		<div class="row content">
+			<h1>Display homepage widgets here</h1>
+		</div>
+
+		<div class="row">
+			<div class="col-md-6">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h3 class="panel-title">Fiche enfant incomplète</h3>
+					</div>
+					<ul class="list-group">
+						<li class="list-group-item"><a href="">Clément Roy</a></li>
+						<li class="list-group-item"><a href="">Christophe Béghin</a></li>
+						<li class="list-group-item"><a href="">Nedjma Behlouli</a></li>
+						<li class="list-group-item"><a href="">Coraline Assimon</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h3 class="panel-title">Panel title</h3>
+					</div>
+					<div class="panel-body">
+						Panel content
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+		<div class="row">
+			<div class="col-md-4">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h3 class="panel-title">Panel title</h3>
+					</div>
+					<div class="panel-body">
+						Panel content
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h3 class="panel-title">Panel title</h3>
+					</div>
+					<div class="panel-body">
+						Panel content
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h3 class="panel-title">Panel title</h3>
+					</div>
+					<div class="panel-body">
+						Panel content
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-12">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h3 class="panel-title">Panel title</h3>
+					</div>
+					<div class="panel-body">
+						Panel content
+					</div>
+				</div>
+			</div>
+		</div>
 
 	</div><!-- /.container -->
 
