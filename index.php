61dd3cc0cd8e36b50db1f5b5236b88cf9a4308cd
ClementRoy <mer02ma>
2013-10-06 16:32:37 +0200

    edit

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+	require 'config/settings.php';
+	require 'config/mysql.php';
+?>
 <!DOCTYPE html>
 <html lang="fr">
   <head>
@@ -32,68 +36,3 @@
     <script src="../../dist/js/bootstrap.min.js"></script>
   </body>
 </html>
-
-=======
-<?php
-	require 'config/settings.php';
-	require 'config/mysql.php';
-?>
-<!doctype html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-	<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
-</head>
-<body>
-<div class="container">
-	<div class="row">
-	  <div class="col-md-4">.col-md-1</div>
-	  <div class="col-md-4">.col-md-1</div>
-	  <div class="col-md-4">.col-md-1</div>
-	</div>
-	<div class="row">
-	  <div class="col-md-4">.col-md-1</div>
-	  <div class="col-md-4">.col-md-1</div>
-	  <div class="col-md-4">.col-md-1</div>
-
-	</div>
-</div>
-
-
-fdfdsf ds ds ds dedit 2
-=======
-bonjour
-=======
-bonjour
-=======
-
-bonjour
-	Gite-des-cimes
-	==============
-
-
-	Gîte des Cîmes
-	tdhdhd
-
-<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-</body>
-</html>
-=======
-
-echo "hello";
-
-?>
-=======
-
-echo "hello";
-
-?>
-=======
-
-echo "hello";
-
-?>
-
-
-bonjour
