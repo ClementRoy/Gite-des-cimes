30b6cab5c37c3d6b95f65ec9516280d0a49b4c62
Christophe Béghin <mail@christophebeghin.fr>
2013-10-06 17:54:25 +0200

    edit path assets

--- parts/header.php
+++ parts/header.php
@@ -7,27 +7,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="shortcut icon" href="img/favicon.png">
+    <link rel="shortcut icon" href="/assets/img/favicon.png">
 
     <title>Gîte des Cîmes - outil de gestion</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="/css/bootstrap.css" rel="stylesheet">
+    <link href="/assets/css/bootstrap.css" rel="stylesheet">
 
-    <link href="/css/bootstrap-theme.css" rel="stylesheet">
+    <link href="/assets/css/bootstrap-theme.css" rel="stylesheet">
 
     <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
 
 	<!--[if IE 7]>
-	  <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome-ie7.min.css">
+	  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome-ie7.min.css">
 	<![endif]-->
 
     <link href="/css/app.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
-      <script src="../../assets/js/html5shiv.js"></script>
-      <script src="../../assets/js/respond.min.js"></script>
+      <script src="/assets/js/html5shiv.js"></script>
+      <script src="/assets/js/respond.min.js"></script>
     <![endif]-->
 	</head>
 	<body>
