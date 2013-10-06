99cf216ebe38360819a7d23a9a276a3260aefdcc
Christophe Béghin <mail@christophebeghin.fr>
2013-10-06 17:51:15 +0200

    a

--- index.php
+++ index.php
@@ -12,13 +12,13 @@
 			<div class="col-md-6">
 				<div class="panel panel-default">
 					<div class="panel-heading">
-						<h3 class="panel-title">Fiche enfant incomplète</h3>
+						<h3 class="panel-title"><span class="glyphicon glyphicon-user"></span> Fiche enfant incomplète</h3>
 					</div>
-					<ul class="list-group">
-						<li class="list-group-item"> <span class="badge">14</span><a href="">Clément Roy</a></li>
-						<li class="list-group-item"><a href="">Christophe Béghin</a></li>
-						<li class="list-group-item"><a href="">Nedjma Behlouli</a></li>
-						<li class="list-group-item"><a href="">Coraline Assimon</a></li>
+
+						<a href="#" class="list-group-item"><span class="badge">14</span>Clément Roy</a>
+						<a href="#" class="list-group-item">Christophe Béghin</a>
+						<a href="#" class="list-group-item">Nedjma Behlouli</a>
+						<a href="#" class="list-group-item">Coraline Assimon</a>
 					</ul>
 				</div>
 			</div>
@@ -75,7 +75,27 @@
 						<h3 class="panel-title">Panel title</h3>
 					</div>
 					<div class="panel-body">
-						Panel content
+						<div class="progress">
+						  <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="100" aria-valuemin="10" aria-valuemax="100" style="width: 30%">
+						    <span class="sr-only">40% Complete (success)</span>
+						  </div>
+						</div>
+						<div class="progress">
+						  <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%">
+						    <span class="sr-only">20% Complete</span>
+						  </div>
+						</div>
+						<div class="progress">
+						  <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
+						    <span class="sr-only">60% Complete (warning)</span>
+						  </div>
+						</div>
+						<div class="progress">
+						  <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+						    <span class="sr-only">80% Complete</span>
+						  </div>
+						</div>
+
 					</div>
 				</div>
 			</div>
