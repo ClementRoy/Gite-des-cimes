eb00bb756cb40e0bc7dd7ae624738ac26c290a5c
ClementRoy <mer02ma>
2013-10-06 16:05:10 +0200

    git pb

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
-<<<<<<< HEAD
-<<<<<<< HEAD
 	require 'config/settings.php';
 	require 'config/mysql.php';
 ?>
@@ -45,10 +43,8 @@ echo "hello";
 echo "hello";
 
 ?>
->>>>>>> ee0546c2da4f1fff4308ffd2c29030eb3aae2ad7
 =======
 
 echo "hello";
 
 ?>
->>>>>>> ee0546c2da4f1fff4308ffd2c29030eb3aae2ad7
