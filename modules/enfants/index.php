ac23dee16ead995f8e7fe72afe7baee0d152bf60
ClementRoy <mer02ma>
2013-10-06 17:10:48 +0200

    modules indexex

---
+++ modules/enfants/index.php
@@ -0,0 +1,13 @@
+<?php require('/parts/header.php'); ?>
+<?php require('/parts/menu.php'); ?>
+
+	<div class="container">
+
+	  <div class="content">
+	    <h1>Display homepage widgets here</h1>
+	  </div>
+
+	</div><!-- /.container -->
+
+
+<?php require('/parts/footer.php'); ?>
