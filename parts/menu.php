df7a66394ef32a6aac9a1987d666ffb7dc2ce7f5
ClementRoy <mer02ma>
2013-10-07 19:22:17 +0200

    changes

--- parts/menu.php
+++ parts/menu.php
@@ -33,11 +33,16 @@ $path_array = explode('/', $_SERVER['SCRIPT_FILENAME']);
 			<li class="dropdown">
 	        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
 	        <ul class="dropdown-menu">
+	        	<li role="presentation" class="dropdown-header">Dropdown header</li>
 	          <li><a href="#">Action</a></li>
 	          <li><a href="#">Another action</a></li>
 	          <li><a href="#">Something else here</a></li>
 	          <li><a href="#">Separated link</a></li>
 	          <li><a href="#">One more separated link</a></li>
+	          
+	          <li role="presentation" class="divider"></li>
+  				<li role="presentation" class="dropdown-header">Dropdown header</li>
+  				<li><a href="#">Another link</a></li>
 	        </ul>
 	      </li>
 
