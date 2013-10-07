81a88fee15d14c73c894e733e2ce511f96821fe7
Christophe Béghin <mail@christophebeghin.fr>
2013-10-07 02:18:24 +0200

    Enfants
    J'ai ajouté un plugin pour trier le tables

--- parts/footer.php
+++ parts/footer.php
@@ -1,14 +1,15 @@
 
 	<div class="container" role="contentinfo">
-		<p>Designed and built with all the ♥ in the world by @C_beghin and @ClementRoy.</p>
+		<p>Designed and built with all the ♥ in the world by @C_Beghin and @ClementRoy.</p>
 	</div>
 
 	<!-- Bootstrap core JavaScript
 	================================================== -->
 	<!-- Placed at the end of the document so the pages load faster -->
-	<script src="/assets/js/jquery.js"></script>
+	<script src="/assets/js/jquery.min.js"></script>
 	<script src="/assets/js/bootstrap.min.js"></script>
-	<script src="/assets/js/app.js"></script>
+	<script src="/assets/js/tinytable.js"></script>
+	<script src="/assets/js/app.min.js"></script>
 
   </body>
 </html>
