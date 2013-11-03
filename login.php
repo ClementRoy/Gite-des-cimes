6e4be1babc0f13eca283a20d39adca929a9b3a28
Clément <royclement@MacBook-Air-de-Clement-2.local>
2013-11-03 02:18:26 +0100

    login

---
+++ login.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html class="login-bg">
+<head>
+    <title>Detail Admin - Sign in</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- bootstrap -->
+    <link href="/assets/css/bootstrap.css" rel="stylesheet">
+    <link href="/assets/css/bootstrap-overrides.css" type="text/css" rel="stylesheet">
+
+    <!-- global styles -->
+    <link rel="stylesheet" type="text/css" href="/assets/css/app.css">
+
+    <!-- libraries -->
+    <link rel="stylesheet" type="text/css" href="/assets/css/lib/font-awesome.css">
+    
+    <!-- open sans font -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+    <div class="login-wrapper">
+        <h1>Gîte des cîmes</h1>
+
+        <div class="box">
+            <div class="content-wrap">
+                <h6>Connexion</h6>
+                <input class="form-control" type="text" placeholder="E-mail address">
+                <input class="form-control" type="password" placeholder="Your password">
+                <a href="#" class="forgot">Mot de passe oublié ?</a>
+                <div class="remember">
+                    <input id="remember-me" type="checkbox">
+                    <label for="remember-me">Se souvenir de moi</label>
+                </div>
+                <a class="btn-glow primary login" href="index.php">Connexion</a>
+            </div>
+        </div>
+
+        <!--
+        <div class="no-account">
+            <p>Don't have an account?</p>
+            <a href="signup.html">Sign up</a>
+        </div>
+        -->
+
+    </div>
+
+    <!-- scripts -->
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="/assets/js/bootstrap.min.js"></script>
+    <script src="/assets/js/theme.js"></script>
+
+</body>
+</html>
