1db55f3a86fdbc311d723bad5becc660e6cc79c6
Clément <royclement@MacBook-Air-de-Clement-2.local>
2013-12-06 22:07:20 +0100

    edit enfant

--- config/config.inc.php
+++ config/config.inc.php
@@ -43,7 +43,7 @@ Certains sont même abstrait
  */
 $modules = array(
 					'accueil' => array( 'index'),
-					'enfants' => array('index','liste','ajouter', 'infos'),
+					'enfants' => array('index','liste','ajouter', 'infos','editer'),
 					'sejours' => array( 'index'),
 					'structures' => array( 'index'),
 					'contacts' => array( 'index'),
