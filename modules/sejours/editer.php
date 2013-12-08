14fed15f954840f9f469e50043c9bbe2372c8a1b
Clément <royclement@MacBook-Air-de-Clement-2.local>
2013-12-08 19:38:45 +0100

    sejours edit and remove

---
+++ modules/sejours/editer.php
@@ -0,0 +1,39 @@
+    <?php require($_SERVER["DOCUMENT_ROOT"] . '/parts/header.php'); ?>
+    <?php require($_SERVER["DOCUMENT_ROOT"] . '/parts/navbar.php'); ?>
+    <?php require($_SERVER["DOCUMENT_ROOT"] . '/parts/menu.php'); ?>
+    <?php //require($_SERVER["DOCUMENT_ROOT"] . '/parts/breadcrumb.php'); ?>
+
+    <?php $sejour = sejour::get($id); ?>
+
+    <?php if(isset($validate)): ?>
+        <?php  
+
+
+        ?>
+    <div class="content">
+        <div id="pad-wrapper" class="form-page">
+            <div class="row header">
+                <div class="col-md-12">
+                    <h3>Modifier un séjour</h3>
+                </div>
+            </div>
+        </div>
+
+        <p>Le séjour  a bien été modifié</p>
+        <a href="/sejours/">Retourner à la liste des séjour</a>
+    </div>
+    <?php else: ?>
+    <div class="content">
+        <div id="pad-wrapper" class="form-page">
+            <div class="row header">
+                <div class="col-md-12">
+                    <h3>Modifier un séjour</h3>
+                </div>
+            </div>
+
+ 
+        </div>
+    </div>
+    <?php endif; ?>
+
+<?php require($_SERVER["DOCUMENT_ROOT"] . '/parts/footer.php'); ?>
---
+++ modules/sejours/supprimer.php
@@ -0,0 +1,50 @@
+    <?php require($_SERVER["DOCUMENT_ROOT"] . '/parts/header.php'); ?>
+    <?php require($_SERVER["DOCUMENT_ROOT"] . '/parts/navbar.php'); ?>
+    <?php require($_SERVER["DOCUMENT_ROOT"] . '/parts/menu.php'); ?>
+    <?php //require($_SERVER["DOCUMENT_ROOT"] . '/parts/breadcrumb.php'); ?>
+
+
+    <!-- main container -->
+    <div class="content">
+        <div id="pad-wrapper" class="action-page">
+            <div class="row header">
+                <div class="col-md-12">
+                    <h3>Séjour</h3>
+                </div>
+            </div>
+            <?php if(isset($confirm)): ?>
+                <div class="row">
+                    <div class="col-md-12">
+                        <?php 
+                        sejour::remove($id);
+                        ?>
+                        <p>Le séjour a bien été supprimé</p>
+                        <a href="/sejours/">Retourner à la liste des séjours</a>
+                    </div>                
+                </div>
+            <?php else: ?>
+                <?php $sejour = sejour::get($id); ?>
+                <div class="row" class="message">
+                    <div class="col-md-12 message">
+                       <p>Vous êtes sur le point de supprimer le séjour <strong><?=$sejour->name; ?> (<?=$sejour->numero; ?>)</strong>.<br />
+                        Cette action est irréversible.</p>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <a href="/sejours/infos/id/<?=$sejour->id; ?>" class="btn-flat white" data-dismiss="modal">Annuler</a>
+                        <a href="/sejours/supprimer/id/<?=$sejour->id; ?>/confirm/true" class="btn-flat danger"><i class="icon-remove"></i> Supprimer</a>
+                    </div>                
+                </div>
+
+
+            <?php endif; ?>
+
+            <!-- end users table -->
+        </div>
+    </div><!-- /.container -->
+    <?php require($_SERVER["DOCUMENT_ROOT"] . '/parts/footer.php'); ?>
+
+
+
