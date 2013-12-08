0229bed99f3d41a3d84f5ff088064aca0244d19c
Christophe Béghin <mail@christophebeghin.fr>
2013-12-08 14:58:00 +0100

    Enfant - Modification fiche

--- modules/enfants/infos.php
+++ modules/enfants/infos.php
@@ -8,7 +8,7 @@
 
         <?php $enfant = enfant::get($id); ?>
 
-        <div id="pad-wrapper">
+        <div id="pad-wrapper" class="user-profile">
                 <div class="row header img">
                     <div class="col-md-5">
                     <img src="http://placehold.it/70x70" class="img-circle" alt="">
@@ -17,8 +17,124 @@
                     <div class="col-md-5 text-right pull-right">
                         <a href="/enfants/remove/id/<?=$enfant->id; ?>" class="btn-flat danger"><i class="icon-remove"></i> Supprimer</a>
                         <a href="/enfants/editer/id/<?=$enfant->id; ?>" class="btn-flat default"><i class="icon-edit"></i> Modifier</a>
+                    </div>
+                </div>
+
+            <div class="row profile">
+                <!-- bio, new note & orders column -->
+                <div class="col-md-9 bio">
+                    <div class="profile-box">
+                        <!-- biography -->
+                        <div class="col-md-12 section">
+                            <h6>Biography</h6>
+                            <p>There are many variations of passages of Lorem Ipsum available but the majority have humour suffered alteration in believable some formhumour , by injected humour, or randomised words which don't look even slightly believable. </p>
+                        </div>
+
+                        <h6>Recent orders</h6>
+                        <br>
+                        <!-- recent orders table -->
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th class="col-md-2">
+                                        Order ID
+                                    </th>
+                                    <th class="col-md-3">
+                                        <span class="line"></span>
+                                        Date
+                                    </th>
+                                    <th class="col-md-3">
+                                        <span class="line"></span>
+                                        Items
+                                    </th>
+                                    <th class="col-md-3">
+                                        <span class="line"></span>
+                                        Total amount
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <!-- row -->
+                                <tr class="first">
+                                    <td>
+                                        <a href="#">#459</a>
+                                    </td>
+                                    <td>
+                                        Jan 03, 2013
+                                    </td>
+                                    <td>
+                                        3
+                                    </td>
+                                    <td>
+                                        $ 3,500.00
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <a href="#">#510</a>
+                                    </td>
+                                    <td>
+                                        Feb 22, 2013
+                                    </td>
+                                    <td>
+                                        5
+                                    </td>
+                                    <td>
+                                        $ 800.00
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <a href="#">#618</a>
+                                    </td>
+                                    <td>
+                                        Jan 03, 2013
+                                    </td>
+                                    <td>
+                                        8
+                                    </td>
+                                    <td>
+                                        $ 3,499.99
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+                        <!-- new comment form -->
+                        <div class="col-md-12 section comment">
+                            <h6>Add a quick note</h6>
+                            <p>Add a note about this user to keep a history of your interactions.</p>
+                            <textarea></textarea>
+                            <a href="#">Attach files</a>
+                            <div class="col-md-12 submit-box pull-right">
+                                <input type="submit" class="btn-glow primary" value="Add Note">
+                                <span>OR</span>
+                                <input type="reset" value="Cancel" class="reset">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- side address column -->
+                <div class="col-md-3 col-xs-12 address pull-right">
+                    <h6>Address</h6>
+                    <iframe width="300" height="133" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com.mx/?ie=UTF8&amp;t=m&amp;ll=19.715081,-155.071421&amp;spn=0.010746,0.025749&amp;z=14&amp;output=embed"></iframe>
+                    <ul>
+                        <li>2301 East Lamar Blvd. Suite 140. </li>
+                        <li>City, Arlington. United States,</li>
+                        <li>Zip Code, TX 76006.</li>
+                        <li class="ico-li">
+                            <i class="ico-phone"></i>
+                            1817 274 2933
+                        </li>
+                         <li class="ico-li">
+                            <i class="ico-mail"></i>
+                            <a href="#">alejandra@detail.com</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
+
 
         <?php tool::output($enfant); ?>
               
