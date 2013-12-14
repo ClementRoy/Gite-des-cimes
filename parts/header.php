7ee283b667c2b47153c36f3cb633ba2b6c338094
Clément <royclement@MacBook-Air-de-Clement-2.local>
2013-12-14 13:01:02 +0100

    with/without google fond

--- parts/header.php
+++ parts/header.php
@@ -27,12 +27,12 @@
     <link href="/assets/css/app.css" rel="stylesheet" type="text/css">
     <link href="/assets/css/lib/font-awesome.css" type="text/css" rel="stylesheet" />
     
-    <!-- open sans font -->
+    <!-- open sans font
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-
-    <!-- lato font -->
+     -->
+    <!-- lato font
     <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
-
+     -->
     <!--[if lt IE 9]>
     <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
