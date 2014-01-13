4ced8cc244f5ba9353ef6c1b899cc26b9fc352d0
Clément <royclement@MacBook-Air-de-Clement-2.local>
2014-01-13 13:53:10 +0100

    Sejours importés

--- scripts/importsejours.php
+++ scripts/importsejours.php
@@ -1,11 +1,17 @@
 <?php  
 
+die();
+
+// Load classes
 // Load classes
 include('../config/mysql.php');
 include('../classes/db.class.php');
+include('../classes/tool.class.php');
 include('../classes/csv.class.php');
+include('../classes/user.class.php');
+include('../classes/sejour.class.php');
 
-
+echo '<html><body style="background:#000;color:#fff">';
 $db = new DB();
 
 $datas = CSV::parse('sejours.csv');
@@ -15,22 +21,36 @@ echo '<pre>';
 foreach ($datas as $key => $data) {
 	//print_r($data);
 	if(!empty($data['1'])){
-		$datasql = array( 'name' => utf8_encode($data['nom']), 
-		 			  	'date_from' => utf8_encode($data['date_debut']),
-						'date_to' => utf8_encode($data['date_fin']),
-						'created' => time(),
-						'place' => utf8_encode($data['lieu']),
-						'capacity_max' => utf8_encode($data['nb_min']),
-						'capacity_min' =>utf8_encode($data['5'])
+
+		$date_from = explode('/', $data['date_from']);
+		$date_to = explode('/', $data['date_to']);
+
+        $metadata = array(
+                            ':created' => tool::currentTime(),
+                            ':edited' => tool::currentTime(),
+                            ':creator' => '1', 
+                            ':editor' => '1', 
+                        );
+
+		$datasql = array( 
+						':name' => utf8_encode($data['nom']), 
+		 			  	':date_from' => tool::currentTime( mktime(0, 0, 0, $date_from['1'], $date_from['0'], $date_from['2'] )),
+						':date_to' => tool::currentTime( mktime(0, 0, 0, $date_to['1'], $date_to['0'], $date_to['2'] )),
+						':place' => utf8_encode($data['place']),
+						':capacity_max' => $data['min'],
+						':capacity_min' =>$data['max'],
+						':price' => $data['8']
 		 			);
-		$sql = 'INSERT INTO sejour (name, date_from, date_to, created, place, capacity_max, capacity_min) value (:name, :date_from, :date_to, :created, :place, :capacity_max, :capacity_min)';
-		$db->insert($sql, $datasql);
-		echo $data['1']."imported \n";
+
+		sejour::add($datasql, $metadata);
+		//$db->insert($sql, $datasql);
+		echo $data['nom']."imported \n";
 	}
 }
 
 
 
+echo '</body></html>';
 
 
 
--- scripts/importstructure.php
+++ scripts/importstructure.php
@@ -21,48 +21,6 @@ foreach ($datas as $key => $data) {
 	//print_r($data);
 	if(!empty($data['0'])){
 
-/*
-    [0] => ASE BEAUMONT
-    [1] => 
-    [2] => 
-    [3] => x
-    [4] => 0
-    [5] => x
-    [6] => 01 34 70 97 63
-    [7] => 01 30 34 03 41
-    [8] => 
-    [9] => 
-    [Structure] => ASE BEAUMONT
-    [Service] => 
-    [numero] => 
-    [AdresseStructure] => x
-    [CPStructure] => 0
-    [VilleStructure] => x
-    [FaxStructure] => 01 34 70 97 63
-    [TelStructure] => 01 30 34 03 41
-    [Observations] => 
-    [email
-
-*/
-
-
-		// $birthdate = explode('/', $data['2']);
-		// $datasql = array( 
-		// 				'created' => tool::currentTime(),
-		// 				'edited' => tool::currentTime(),
-		// 			    'firstname' => utf8_encode($data['1']), 
-		// 			    'lastname' => utf8_encode($data['0']),
-		// 			    'birthdate' => tool::currentTime( mktime(0, 0, 0, $birthdate['1'], $birthdate['0'], $birthdate['2'] )),
-		// 			    'number_ss' => $data['11'],
-		// 			    'note' => 'numero caf :'.$data['10']."\n".$data['19']."\n".$data['8'],
-		// 			    'father_name' => $data['14'],
-		// 			    'mother_name' => $data['15'],
-		// 			    'father_phone' => $data['9']
-		// 			);
-		// $sql = 'INSERT INTO enfant (created, edited, firstname, lastname, birthdate, number_ss, note, father_name, mother_name, father_phone) 
-		// 					value (:created, :edited, :firstname, :lastname, :birthdate, :number_ss, :note, :father_name, :mother_name, :father_phone)';
-		// $db->insert($sql, $datasql);
-
         $metadata = array(
                             ':created' => tool::currentTime(),
                             ':edited' => tool::currentTime(),
