3fd34a2b4863b0ab1015b4b09e56583cc2496452
Clément <royclement@MacBook-Air-de-Clement-2.local>
2014-02-12 01:49:40 +0100

    date_default_timezone_set

--- config/config.inc.php
+++ config/config.inc.php
@@ -17,6 +17,8 @@ ini_set('log_errros', 1);
 ini_set('error_log', dirname(__FILE__) . '/error.log');
 error_reporting(E_ALL);
 
+date_default_timezone_set('Europe/Paris');
+
 /*
 Fonction d'autoload des classes PHP
 Toutes les classes doivent être dans le dossiers /classes
