40023baad1f7b0d4dd6d2be3f1d4502ec0c15592
Christophe Béghin <mail@christophebeghin.fr>
2014-03-07 12:26:59 +0100

    bonjour

--- modules/utilisateurs/connexion.php
+++ modules/utilisateurs/connexion.php
@@ -35,22 +35,17 @@
                 <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
                 <li data-target="#carousel-example-generic" data-slide-to="1"></li>
                 <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="3"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="4"></li>
             </ol>
 
             <div class="carousel-inner">
                 <div class="item active">
-                    <img src="/assets/img/bgs/10.jpg" alt="...">
+                    <img src="/assets/img/bgs/bg1.jpg" alt="...">
                 </div>
                 <div class="item">
-                    <img src="/assets/img/bgs/11.jpg" alt="...">
+                    <img src="/assets/img/bgs/bg2.jpg" alt="...">
                 </div>
                 <div class="item">
-                    <img src="/assets/img/bgs/12.jpg" alt="...">
-                </div>
-                <div class="item">
-                    <img src="/assets/img/bgs/13.jpg" alt="...">
+                    <img src="/assets/img/bgs/bg3.jpg" alt="...">
                 </div>
             </div>
 
