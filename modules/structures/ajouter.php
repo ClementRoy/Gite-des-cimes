b62067f36aea69f0a115efc8e9d12dc420d07e27
ClementRoy <mail@clementroy.fr>
2014-12-21 18:41:37 +0100

    Ajouter un contact sur la fiche structure à la volée

--- modules/structures/ajouter.php
+++ modules/structures/ajouter.php
@@ -117,6 +117,95 @@
                         </div>
                     </div>
 
+                    <div class="field-box row">
+                        <div class="col-md-5 col-md-offset-2">
+                             <a href="#add_contact" id="add_contact" class="btn btn-default">Ajouter les coordonnées d'un contact ?</a>
+                        </div>
+                    </div>
+
+                    <div class="hide" id="panel_add_contact">
+                        <div class="field-box row">
+                            <label class="col-md-2">Civilité</label>
+                            <div class="col-md-4" data-toggle="tooltip" title="Précisez la civilité du contact">
+                                <label class="radio-inline col-md-7" for="form-contact-civility-mme">
+                                    <div class="radio" id="uniform-form-contact-civility-mme">
+                                        <span class="checked">
+                                            <input type="radio" name="form_contact_civility" id="form-contact-civility-mme" value="Mme">
+                                        </span>
+                                    </div>
+                                    Mme
+                                </label>
+                                <label class="radio-inline col-md-4" for="form-contact-civility-mr">
+                                    <div class="radio" id="uniform-form-contact-civility-mr">
+                                        <span class="checked">
+                                            <input type="radio" name="form_contact_civility" id="form-contact-civility-mr" value="Mr" checked="">
+                                        </span>
+                                    </div>
+                                    Mr
+                                </label>
+                            </div>
+                        </div>
+
+                        <div class="field-box row">
+                            <label class="col-md-2" for="form-contact-prenom">Prénom</label>
+                            <div class="col-md-4 col-sm-5">
+                                <input id="form-contact-prenom" name="form_contact_firstname" class="form-control input-sm" type="text" data-toggle="tooltip" title="Renseignez le prénom du contact.">
+                            </div>
+                        </div>
+
+                        <div class="field-box row">
+                            <label class="col-md-2" for="form-contact-nom">Nom</label>
+                            <div class="col-md-4 col-sm-5">
+                                <input id="form-contact-nom" name="form_contact_lastname" class="form-control input-sm input-sm" type="text" data-toggle="tooltip" title="Renseignez le nom du contact." parsley-required="true">
+                            </div>
+                        </div>
+
+                        <div class="field-box row">
+                            <label class="col-md-2" for="form-contact-title">Titre</label>
+                            <div class="col-md-4">
+                                <input id="form-contact-title" name="form_contact_title" class="form-control" type="text" 
+                                data-toggle="tooltip" title="Renseignez le titre de la contact." parsley-required="true">
+                            </div>
+                        </div>
+
+                        <div class="field-box row">
+                            <label class="col-md-2" for="form-contact-email">Email</label>
+                            <div class="col-md-4">
+                                <input id="form-contact-email" name="form_contact_email" type="text" class="form-control"
+                                data-toggle="tooltip" title="Renseignez l'email de la contact." 
+                                parsley-type="email">
+                            </div>
+                        </div>     
+
+                        <div class="field-box row">
+                            <label class="col-md-2" for="form-contact-phone">Téléphone</label>
+                            <div class="col-md-4">
+                                <input id="form-contact-phone" name="form_contact_telephone" type="text" class="form-control"
+                                data-toggle="tooltip" title="Renseignez le numéro de téléphone de la contact." 
+                                parsley-type="phone">
+                            </div>
+                        </div>
+
+                        <div class="field-box row">
+                            <label class="col-md-2" for="form-contact-mobile-phone">Téléphone mobile</label>
+                            <div class="col-md-4">
+                                <input id="form-contact-mobile-phone" name="form_contact_mobile_phone" type="text" class="form-control"
+                                data-toggle="tooltip" title="Renseignez le numéro de téléphone mobile du contact.">
+                            </div>
+                        </div>
+
+                        <div class="field-box row">
+                            <label class="col-md-2" for="form-contact-fax">Fax</label>
+                            <div class="col-md-4">
+                                <input id="form-contact-fax" name="form_contact_fax" type="text" class="form-control"
+                                data-toggle="tooltip" title="Renseignez le numéro de fax de la contact." 
+                                parsley-type="phone">
+                                </div>
+                        </div>                        
+
+
+                    </div>
+                   
 
                     <div class="field-box actions">
                         <div class="col-md-6  col-md-offset-2">
@@ -131,4 +220,13 @@
         </div>
 
 </div>
+
+<script>
+    $(function () {
+        $('#add_contact').click(function(e){
+            e.preventDefault();
+            $('#panel_add_contact').removeClass('hide');
+        })
+    });
+</script>
 <?php require($_SERVER["DOCUMENT_ROOT"] . '/parts/footer.php'); ?>
