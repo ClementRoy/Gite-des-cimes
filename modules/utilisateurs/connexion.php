e29425cea7cea00b82b18ead631293a730c65f93
Christophe Béghin <christophe@MacBook-Pro-de-Christophe-2.local>
2016-08-12 15:21:28 +0200

    Worked on responsive

--- modules/utilisateurs/connexion.php
+++ modules/utilisateurs/connexion.php
@@ -7,16 +7,20 @@
     <title>Gîte des Cîmes - Outil de gestion</title>
     <meta name="robots" CONTENT="noindex, nofollow">
     
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,700italic,400,600,700,300">
+    <!-- <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,700italic,400,600,700,300"> -->
 
     <?php if (APP_VERSION != 'dev'): ?>
+
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 
     <link rel="stylesheet" href="/assets/css/gitedescimes.min.css" />
     
     <?php else: ?>
 
+    <link rel="stylesheet" href="/assets/libs/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="/assets/libs/font-awesome/css/font-awesome.css">
+    
     <link rel="stylesheet" href="/assets/libs/jquery.easy-pie-chart/jquery.easy-pie-chart.css">
     <link rel="stylesheet" href="/assets/libs/odometer/themes/odometer-theme-default.css">
     <link rel="stylesheet" href="/assets/css/datatables-bootstrap-adapter.css">
